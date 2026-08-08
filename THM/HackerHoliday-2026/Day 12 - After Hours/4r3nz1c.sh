fc0377b3ce576d796654b43684b8bae3b468aede
Lahfen Brandy <173473111+stormk1n@users.noreply.github.com>
2026-08-08 16:23:59 +0100

    Update 4r3nz1c.sh

--- THM/HackerHoliday-2026/Day 12 - After Hours/4r3nz1c.sh
+++ THM/HackerHoliday-2026/Day 12 - After Hours/4r3nz1c.sh
@@ -5,14 +5,14 @@
 # sort the strings for unique entries of JAB
 pwshLder=$(strings OBJECTS.DATA | grep "JAB" | sed -E 's/.*-enc\s+//' | sort -u | base64 -d | tr -d '\0')
 
-printf "[+] Powershell Loader \n\n $pwshLder\n\n"
+printf "[+] Powershell Loader \n$pwshLder\n\n"
 
 # using python zlib with a window size of '-15' to force decompression
 # of a raw headerless .NET DeflateStream (7VZ**** encoded string), saving the output as an executable binary.
 payloadBinfile=$(strings OBJECTS.DATA | grep "7VZ"  | base64 -d | python3 -c "import sys, zlib; sys.stdout.buffer.write(zlib.decompress(sys.stdin.buffer.read(), -15))" > payload.bin)
 
 file=$(file payload.bin)
-printf "[+] File Type \n$file\n"
+printf "[+] File Type \n $file\n"
 
 # Extract UTF-16LE strings looking for the backdoor command
 cmdString=$(strings -e l payload.bin | grep -i "net user")
