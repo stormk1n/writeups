d57fb5750c54c2e4b235db1a37d95f30fdcea283
Lahfen Brandy <173473111+stormk1n@users.noreply.github.com>
2026-08-08 16:04:18 +0100

    Add files via upload

---
+++ THM/HackerHoliday-2026/Day 12 - After Hours/4r3nz1c.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# first we extract readable strings
+# from the OJBECT.DATA file
+# sort the strings for unique entries of JAB
+pwshLder=$(strings OBJECTS.DATA | grep "JAB" | sed -E 's/.*-enc\s+//' | sort -u | base64 -d | tr -d '\0')
+
+printf "[+] Powershell Loader \n\n $pwshLder\n\n"
+
+# using python zlib with a window size of '-15' to force the decompression 
+# of a raw headerless .NET DeflateStream, saving the output as an executable binary.
+payloadBinfile=$(strings OBJECTS.DATA | grep "7VZ"  | base64 -d | python3 -c "import sys, zlib; sys.stdout.buffer.write(zlib.decompress(sys.stdin.buffer.read(), -15))" > payload.bin)
+
+file=$(file payload.bin)
+printf "[+] File Type \n$file\n"
+
+# Extract UTF-16LE strings looking for the backdoor command
+cmdString=$(strings -e l payload.bin | grep -i "net user")
+printf "\n[+] Backdoor Command:\n $cmdString\n"
+
+# Isolate the Base64 password blob and decode it
+b64Password=$(echo "$cmdString" | awk '{print $5}')
+flag=$(echo "$b64Password" | base64 -d)
+
+printf "\n[+] R00M Fl4g :\n $flag\n"
+
---
+++ THM/HackerHoliday-2026/Day 12 - After Hours/method.md
@@ -0,0 +1,21 @@
+# Getting started
+
+Download the attachement file and unzip with
+```bash
+unzip attachments-1784136288483.zip
+```
+This lab shows up a classic THM forensic challenge
+
+# Crafting our extraction tool
+After analyzing the file OBJECTS.DATAs, we Write a little bash script to do our dirty work
+
+use the 4r3nz1c.sh script to get the flag as
+```
+chmod +x ./4r3nz1c.sh \
+./4z3nzic.sh
+```
+Room flag
+```
+[+] R00M Fl4g :
+ THM{===---==--//--===--:)-==}
+```
