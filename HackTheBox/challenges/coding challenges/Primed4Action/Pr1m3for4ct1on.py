5bfc6df071e8abad488fdda64c68d931c0fe0531
Lahfen Brandy <173473111+stormk1n@users.noreply.github.com>
2026-08-24 22:14:50 +0100

    Update Pr1m3for4ct1on.py

--- HackTheBox/challenges/coding challenges/Primed4Action/Pr1m3for4ct1on.py
+++ HackTheBox/challenges/coding challenges/Primed4Action/Pr1m3for4ct1on.py
@@ -1,23 +1,26 @@
 import math
-
-# Take the input
+# take in the number
 chalVal = input()
 nums = [int(val) for val in chalVal.split(" ")]
 
-
-# Check if the number is prime
+# calculate answer
 def isPrime(n: int) -> bool:
-  if n <= 1:
-    return False
-  maxFctr = int(isqrt(n))
-  for i in range(3, maxFctr +1):
-    if n % i == 0:
-      return False
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    maxFctr = int(math.isqrt(n))
+    for i in range(3, maxFctr + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 primeNums = [num for num in nums if isPrime(num)]
 
 if len(primeNums) == 2:
-  prdct = primeNums[0] * primeNums [1]
+    prdct = primeNums[0] * primeNums[1]
 
-# printing the output
+# print answer
 print(prdct)
