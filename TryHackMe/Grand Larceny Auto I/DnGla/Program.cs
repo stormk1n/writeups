10941bcb17c749b290fcf7bf5019a92cc710bb40
Lahfen Brandy <173473111+stormk1n@users.noreply.github.com>
2026-08-29 02:44:27 +0100

    Add files via upload

---
+++ TryHackMe/Grand Larceny Auto I/DnGla/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GrandLarcenyAuto;
+
+namespace DNGLA
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     Console.WriteLine("Starting hiest");
+     PlayerState player = new PlayerState();
+     player.WantedStars = 6;
+     
+     Console.WriteLine("Stars set to six")
+     SafehouseVault vault = new SafehouseVault(player);
+     Console.WriteLine(vault.TryOpen())
+     }
+   }
+}
